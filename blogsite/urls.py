6af275d7a05040a153a42673c591bad493b4aaec
David Hines <david.hines.01@gmail.com>
2015-06-24 11:05:32 -0300

    Added the posts to the homepage.

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'blogsite.views.home', name='home'),
-    url(r'', include('blogengine.urls')),
+    url(r'.*', include('blogengine.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
