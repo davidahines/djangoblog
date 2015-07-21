15c838f6c42b0627c78484535aaf52daa10d90e0
David Hines <david.hines.01@gmail.com>
2015-07-21 14:37:09 -0300

    Edited timezone to use datetime.datetime.now() as that is compatible with 1.3

--- blogengine/models.py
+++ blogengine/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from django.utils import datetime
 # Create your models here.
 
 class Post(models.Model):
@@ -7,12 +7,12 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
-            default=timezone.now)
+            default=datetime.datetime.now())
     published_date = models.DateTimeField(
             blank=True, null=True)
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = datetime.datetime.now()
         self.save()
 
     def __str__(self):
--- blogengine/views.py
+++ blogengine/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
+from django.utils import datetime
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=datetime.datetime.now()).order_by('published_date')
     return render(request, 'blogengine/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
