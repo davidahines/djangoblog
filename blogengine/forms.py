1f1d725db98f75366eb472566d018fc0f0b864db
David Hines <david.hines.01@gmail.com>
2015-08-23 00:49:02 -0300

    Ugly copy, nav isn't continuing to the bottom. Made a contact page though, so that's kinda nice. Doesn't get processed though, that's for later.

--- blogengine/forms.py
+++ blogengine/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class ContactForm(forms.Form):
+        contact_name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'class':'basic_input'}))
+        contact_email = forms.CharField(label='Email', max_length=100, widget=forms.TextInput(attrs={'class':'basic_input'}))
+        contact_subject = forms.CharField(label='Subject', max_length=100, widget=forms.TextInput(attrs={'class':'basic_input'}))
+        contact_message = forms.CharField(label='Message', max_length=5000, widget=forms.Textarea(attrs={'class':'basic_input'}))
--- blogengine/views.py
+++ blogengine/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 import datetime
 from .models import Post
+from .forms import ContactForm
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +11,21 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blogengine/post_detail.html', {'post': post})
+
+def contact_form(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ContactForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'blogengine/contact.html', {'form': form})
